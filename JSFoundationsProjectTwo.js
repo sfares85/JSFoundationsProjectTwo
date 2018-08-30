741d1cb6048e8c4f2229584c394b56f9ec28485b
darkwight <m.alsharekh94@gmail.com>
2018-08-30 18:14:49 +0300

    Solution code

--- JSFoundationsProjectTwo.js
+++ JSFoundationsProjectTwo.js
@@ -1,4 +1,5 @@
 // BONUS: Do sumOdds() without using the filterOdds() function and without using a .filter() method.
+// BONUS: Do the functions logger() toCelsius() hottestDays() logHottestDays() each in a single line.
 
 /**
  * Receives an array of numbers
@@ -32,44 +33,81 @@ function sumOdds(numbers) {
 }
 
 /**
- * Receives an array of names
- * Makes the array into an array of pairs
- * Order of names and pairs doesn't matter
- * if the length of the array is odd, the last element is in an array on its own
- * Returns the array of pairs
+ * Receives an array
+ * Logs every element of the array
  */
-function makePairs(names) {
-	let result = [];
-	let pairs = [];
+const logger = function(array) {
+  array.forEach(element => console.log(element));
+};
 
-	while(names.length > 1) {
-		pairs.push(names.pop());
-		pairs.push(names.pop());
-		result.push(pairs);
-		pairs = [];
-	}
+/**
+ * Receives an array of temperatures in degrees Fahrenheit
+ * Returns an array of temperatures in degrees Celsius
+ *
+ * The conversion formula is:
+ *   C = (F - 32) * (5/9)
+ */
+const toCelsius = function(temperatures) {
+  return temperatures.map(t => (t - 32) * (5 / 9));
+};
 
-	if (names.length) {
-		result.push([names.pop()])
-	}
+/**
+ * Receives an array of temperatures and a threshold temperature
+ * Returns an array of temperatures that exceed the threshold
+ */
+const hottestDays = function(temperatures, threshold) {
+  return temperatures.filter(t => t > threshold);
+};
 
-	return result;
-}
+/**
+ * Receives an array of temperatures IN DEGREES FAHRENHEINT and a threshold temperature IN DEGREES FAHRENHEINT
+ * Logs temperatures that exceed the threshold to the console IN DEGREES CELSIUS
+ *
+ * hint: you can combine previous functions
+ */
+const logHottestDays = function(temperatures, threshold) {
+  logger(toCelsius(hottestDays(temperatures, threshold)));
+};
 
 /**************************************************
 The following code runs the functions defined above
 ***************************************************/
 let numbers = [153,67,9,34,7,67,342,7,34,8656,2,124,5,43];
-// let instructors = ["Hamza", "Mshary", "Aziz", "Hussein", "Fawaz"];
+let instructors = ["Hamza", "Mshary", "Aziz", "Hussein", "Fawaz"];
+let temperatures = [32,212,-40,122,54.5];
+let temperaturesForThreshold = [0,-5,35,20,45,50,10];
+let threshold = 40;
+hottestDays([1,2,3,4,5],0);
 
-// // filterEvens
-// evens = filterEvens(numbers);
-// console.log(evens);
+// filterEvens
+evens = filterEvens(numbers);
+console.log("EVENS:");
+console.log(evens);
 
 // sumOdds
 sum = sumOdds(numbers);
+console.log("\n--------------------------")
+console.log("SUM ODDS:");
 console.log(sum);
 
-// // makePairs
-// let pairs = makePairs(instructors);
-// console.log(pairs);
+// logger
+console.log("\n--------------------------")
+console.log("LOGGER:");
+logger(instructors);
+
+// toCelsius
+let tempsInCelsius = toCelsius(temperatures);
+console.log("\n--------------------------")
+console.log("TO CELSIUS:");
+console.log(tempsInCelsius);
+
+// hottestDays
+let veryHotDays = hottestDays(temperaturesForThreshold, threshold);
+console.log("\n--------------------------")
+console.log("HOTTEST DAYS:");
+console.log(veryHotDays);
+
+// logHottestDays
+console.log("\n--------------------------")
+console.log("LOG HOTTEST DAYS:");
+logHottestDays(temperaturesForThreshold, threshold);
